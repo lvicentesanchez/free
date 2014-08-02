fc0bbfef2eb6b38157fa520f609fe45cddcec8ec
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2014-08-02 01:32:41 +0100

    Minor refactoring.

--- src/main/scala/services/modules/interpreter/Interpreter.scala
+++ src/main/scala/services/modules/interpreter/Interpreter.scala
@@ -3,8 +3,8 @@ package services.modules.interpreter
 import scalaz.{ ~>, Monad, Free, Functor }
 
 trait Interpreter[F[_], M[_]] {
-  def apply[A](input: Free[F, A])(implicit F: Functor[F], M: Monad[M], N: ~>[F, M]): M[A] =
-    input.runM[M](N.apply[Free[F, A]])
+  def apply[A](input: Free[F, A])(implicit F: Functor[F], M: Monad[M], f: F ~> M): M[A] =
+    input.runM[M](f[Free[F, A]])
 }
 
 object Interpreter {
