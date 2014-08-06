a97f0aacaf769e8d3d3c11397f2ff7eb68af5507
Luis Angel Vicente Sanchez <luis.vicente@mindcandy.com>
2014-08-06 18:16:13 +0100

    Make the interpreter a sealed trait.

--- src/main/scala/services/modules/interpreter/Interpreter.scala
+++ src/main/scala/services/modules/interpreter/Interpreter.scala
@@ -2,7 +2,7 @@ package services.modules.interpreter
 
 import scalaz.{ ~>, Monad, Free, Functor }
 
-trait Interpreter[F[_], M[_]] {
+sealed trait Interpreter[F[_], M[_]] {
   def apply[A](input: Free[F, A])(implicit F: Functor[F], M: Monad[M], f: F ~> M): M[A] =
     input.runM[M](f[Free[F, A]])
 }
